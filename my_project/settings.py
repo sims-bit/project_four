598a9b02a619829f0e798b94a9bcf85b73bf3213
sims-bit <xfiqoe46@students.codeinstitute.net>
2025-06-30 11:18:09 +0100

    delete type specification. update debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1', #vs code preview
